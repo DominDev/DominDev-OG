3cb345c1a06ba8a542fbce7d11f8465994e753cd
DominDev <pawel.dominiak@domindev.com>
2025-12-19 23:21:31 +0100

    maps

--- src/js/config.js
+++ src/js/config.js
@@ -19,13 +19,13 @@ const CONFIG = {
   ADDRESS: 'Klub Sportowy Awangarda 71, ul. Ołbińska 17/budynek B, 50-237 Wrocław',
   GYM_NAME: 'Awangarda 71 Gym',
 
-  // Google Maps (centered on Awangarda 71, interaction disabled via CSS)
-  MAPS_EMBED: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1252.3!2d17.0383!3d51.11535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fc2760c33218f%3A0xf64d5baa498dff4f!2sAwangarda%2071!5e0!3m2!1spl!2spl!4v1703001234567',
-  MAPS_NAV: 'https://maps.app.goo.gl/mAgjtGAqKttSf7v19',
+  // Google Maps (centered on training location, interaction disabled via CSS)
+  MAPS_EMBED: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2504.1608866930846!2d17.035511377206173!3d51.12394033864306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fc33067495a7f%3A0xfb749caf4a5b2287!2sBloom%20Kick-Boxing%20Club!5e0!3m2!1spl!2spl!4v1766182314881!5m2!1spl!2spl',
+  MAPS_NAV: 'https://maps.app.goo.gl/v9jnNpMmD6BE6VHF6',
 
-  // Coordinates for schema.org
-  LATITUDE: '51.114757',
-  LONGITUDE: '17.037553'
+  // Coordinates for schema.org (ul. Ołbińska 17/B)
+  LATITUDE: '51.12394',
+  LONGITUDE: '17.03551'
 };
 
 // Generate WhatsApp URL
